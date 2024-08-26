e08a7d684ce9720953da16e3de4433f31d7b5efa
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-26 17:14:41 +0100

    Feat: Post User endpoint set up in user controller.

--- src/main/java/com/jackson/personal_finance_manager/controller/UserController.java
+++ src/main/java/com/jackson/personal_finance_manager/controller/UserController.java
@@ -1,9 +1,22 @@
 package com.jackson.personal_finance_manager.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jackson.personal_finance_manager.dto.userdtos.UserRegistrationDTO;
+import com.jackson.personal_finance_manager.model.User;
+import com.jackson.personal_finance_manager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserRegistrationDTO user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
 }
