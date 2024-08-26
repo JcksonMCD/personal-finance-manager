78827fe73a56a14ab3649cd621382e78ce8950c6
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-26 16:17:38 +0100

    Feat: Validation anotations added to user class.

--- src/main/java/com/jackson/personal_finance_manager/model/User.java
+++ src/main/java/com/jackson/personal_finance_manager/model/User.java
@@ -1,11 +1,40 @@
 package com.jackson.personal_finance_manager.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 public class User {
-    String username;
-    String name;
-    String email;
-    String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserID")
+    private Long userId;
+
+    @Column(name = "Username", unique = true, nullable = false)
+    @NotNull
+    @Size(max = 50)
+    private String username;
+
+    @Column(name = "Email", unique = true, nullable = false)
+    @NotNull
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    @Column(name = "PasswordHash", nullable = false)
+    @NotNull
+    private String passwordHash;
+
+    @Column(name = "CreatedAt", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "UpdatedAt")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
