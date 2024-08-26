0c8c188f94a72441f5d8e950c370bb523d266216
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-26 17:27:04 +0100

    Feat: Security Config class complete with BCryptencoder set up.

--- src/main/java/com/jackson/personal_finance_manager/service/UserServiceImpl.java
+++ src/main/java/com/jackson/personal_finance_manager/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.jackson.personal_finance_manager.dto.userdtos.UserRegistrationDTO;
 import com.jackson.personal_finance_manager.model.User;
 import com.jackson.personal_finance_manager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,9 @@ import java.time.LocalDateTime;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public User createUser(UserRegistrationDTO userRegistrationDTO) {
