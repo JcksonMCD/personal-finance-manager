6d3f470a1e6afa778237b70e7bb4eb198bed53c8
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-27 11:41:33 +0100

    Feat: Test file set up and annotations complete.

--- src/test/java/com/jackson/personal_finance_manager/repository/UserRepositoryTest.java
+++ src/test/java/com/jackson/personal_finance_manager/repository/UserRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.jackson.personal_finance_manager.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-class UserRepositoryTest {
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
 
 }
