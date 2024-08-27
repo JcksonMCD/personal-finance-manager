276e8c98203ebe0f246bf2e52b93993ab64a2a41
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-27 11:33:56 +0100

    Fix: Temp security config set up to allow for testing will in dev stage.

--- src/main/java/com/jackson/personal_finance_manager/config/SecurityConfig.java
+++ src/main/java/com/jackson/personal_finance_manager/config/SecurityConfig.java
@@ -1,12 +1,38 @@
 package com.jackson.personal_finance_manager.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Enable logout functionality
+        http
+                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF protection for simplicity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users").permitAll() // Public endpoints
+                        .anyRequest().authenticated() // Require authentication for all other requests
+                )
+                .formLogin(form -> form // Enable form-based login
+                        .loginPage("/login") // Custom login page endpoint
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
+                );
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
