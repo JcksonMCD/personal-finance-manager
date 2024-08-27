9a0210b33e55ceecf56199188d9fb27a9d81f93f
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-27 11:12:10 +0100

    Feat: Mocking and test set up for user controller test file complete.

--- src/test/java/com/jackson/personal_finance_manager/controller/UserControllerTest.java
+++ src/test/java/com/jackson/personal_finance_manager/controller/UserControllerTest.java
@@ -1,10 +1,24 @@
 package com.jackson.personal_finance_manager.controller;
 
+import com.jackson.personal_finance_manager.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
 
     @Test
     void createUser() {
