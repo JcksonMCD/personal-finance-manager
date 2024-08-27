ac84426001949e612b9f153c0ac225ec9f808c8c
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-27 15:32:09 +0100

    Feat: Mock set up done for user service impl test file.

--- src/test/java/com/jackson/personal_finance_manager/service/UserServiceImplTest.java
+++ src/test/java/com/jackson/personal_finance_manager/service/UserServiceImplTest.java
@@ -1,10 +1,29 @@
 package com.jackson.personal_finance_manager.service;
 
+import com.jackson.personal_finance_manager.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceImplTest {
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void createUser() {
