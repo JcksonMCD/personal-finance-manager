af17274e12b0c8d1eb5160d77162db1b50e28a80
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-02 17:35:28 +0100

    Feat: Plaid controller created to handle plaid related endpoints.

---
+++ src/main/java/com/jackson/personal_finance_manager/controller/PlaidController.java
@@ -0,0 +1,12 @@
+package com.jackson.personal_finance_manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/plaid")
+public class PlaidController {
+
+}
+
