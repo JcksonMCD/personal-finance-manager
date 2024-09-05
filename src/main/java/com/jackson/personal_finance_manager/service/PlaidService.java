f64302631222e308a75456ff21d8f1a40533b1ee
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-05 15:56:34 +0100

    Feat: Get transactions method added to plaid service interface.

--- src/main/java/com/jackson/personal_finance_manager/service/PlaidService.java
+++ src/main/java/com/jackson/personal_finance_manager/service/PlaidService.java
@@ -2,12 +2,15 @@ package com.jackson.personal_finance_manager.service;
 
 
 import com.plaid.client.response.AuthGetResponse;
+import com.plaid.client.response.TransactionsGetResponse;
 
 import java.io.IOException;
+import java.util.Date;
 
 public interface PlaidService {
     String exchangePublicToken(String publicToken) throws IOException;
 
     AuthGetResponse getAccountInfo() throws IOException;
 
+    TransactionsGetResponse getTransactions(Date startDate, Date endDate) throws IOException;
 }
