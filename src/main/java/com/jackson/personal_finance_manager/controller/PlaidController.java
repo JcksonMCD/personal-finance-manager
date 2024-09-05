4fc14ff31300c19fdb24aeaa04e628bc1f305f5b
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-05 15:32:59 +0100

    Feat: Acces token get endpoint added to plaid controller file.

--- src/main/java/com/jackson/personal_finance_manager/controller/PlaidController.java
+++ src/main/java/com/jackson/personal_finance_manager/controller/PlaidController.java
@@ -2,8 +2,16 @@ package com.jackson.personal_finance_manager.controller;
 
 import com.jackson.personal_finance_manager.service.PlaidService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 
 
@@ -16,6 +24,19 @@ public class PlaidController {
     @Autowired
     public PlaidController(PlaidService plaidService) {
         this.plaidService = plaidService;
+    }
+
+    @RequestMapping(value="/get_access_token", method=POST, consumes=MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public @ResponseBody ResponseEntity getAccessToken(@RequestParam("public_token") String publicToken) {
+        try {
+            String accessToken = plaidService.exchangePublicToken(publicToken);
+            Map<String, Object> data = new HashMap<>();
+            data.put("error", false);
+            data.put("access_token", accessToken);
+            return ResponseEntity.ok(data);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error exchanging token");
+        }
     }
 }
 
