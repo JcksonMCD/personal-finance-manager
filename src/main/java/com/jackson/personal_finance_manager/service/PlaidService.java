adcdc8f74e08c5af61e88c861287ff66cbc8466f
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-05 15:50:53 +0100

    Feat: Get Account info method added to plaid service interface.

--- src/main/java/com/jackson/personal_finance_manager/service/PlaidService.java
+++ src/main/java/com/jackson/personal_finance_manager/service/PlaidService.java
@@ -1,8 +1,13 @@
 package com.jackson.personal_finance_manager.service;
 
 
+import com.plaid.client.response.AuthGetResponse;
+
 import java.io.IOException;
 
 public interface PlaidService {
     String exchangePublicToken(String publicToken) throws IOException;
+
+    AuthGetResponse getAccountInfo() throws IOException;
+
 }
