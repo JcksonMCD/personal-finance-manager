851788be2df0a69702de594b370d9221dbc3ad19
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-05 15:43:47 +0100

    Feat: Plaid service impl set up with a client and auth service field and autowired constructor.

--- src/main/java/com/jackson/personal_finance_manager/service/PlaidServiceImpl.java
+++ src/main/java/com/jackson/personal_finance_manager/service/PlaidServiceImpl.java
@@ -1,7 +1,18 @@
 package com.jackson.personal_finance_manager.service;
 
+import com.plaid.client.PlaidClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlaidServiceImpl implements PlaidService{
+
+    PlaidClient plaidClient;
+    PlaidAuthService plaidAuthService;
+
+    @Autowired
+    public PlaidServiceImpl(PlaidAuthService plaidAuthService, PlaidClient plaidClient) {
+        this.plaidAuthService = plaidAuthService;
+        this.plaidClient = plaidClient;
+    }
 }
