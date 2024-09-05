60f6b0b31f8559f4c8fb8dca1614d45ce1f29f90
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-05 15:36:28 +0100

    Feat: Plaid Auth Service class created to store token data.

---
+++ src/main/java/com/jackson/personal_finance_manager/service/PlaidAuthService.java
@@ -0,0 +1,8 @@
+package com.jackson.personal_finance_manager.service;
+
+import org.springframework.stereotype.Service;
+
+// For storing plaid service tokens
+@Service
+public class PlaidAuthService {
+}
