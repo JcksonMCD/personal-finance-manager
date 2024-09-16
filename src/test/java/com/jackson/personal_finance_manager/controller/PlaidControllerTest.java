5c3b8de2a64a5dbda446eafc225a2e5373cb1492
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-16 17:01:52 +0100

    Feat: Test file created for plaid controller class.

---
+++ src/test/java/com/jackson/personal_finance_manager/controller/PlaidControllerTest.java
@@ -0,0 +1,20 @@
+package com.jackson.personal_finance_manager.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaidControllerTest {
+
+    @Test
+    void getAccessToken() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+
+    @Test
+    void getTransactions() {
+    }
+}
