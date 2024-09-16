a5bff6076754bf8208cc50956b4c51a380d7f78d
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-16 17:43:05 +0100

    Feat: Test file set up created for plaid service impl.

--- src/test/java/com/jackson/personal_finance_manager/service/PlaidServiceImplTest.java
+++ src/test/java/com/jackson/personal_finance_manager/service/PlaidServiceImplTest.java
@@ -1,20 +1,28 @@
 package com.jackson.personal_finance_manager.service;
 
+import com.plaid.client.PlaidClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import static org.junit.jupiter.api.Assertions.*;
+@SpringJUnitConfig
+public class PlaidServiceImplTest {
 
-class PlaidServiceImplTest {
+    @Mock
+    private PlaidClient plaidClient;
 
-    @Test
-    void exchangePublicToken() {
+    @Mock
+    private PlaidAuthService plaidAuthService;
+
+    @InjectMocks
+    private PlaidServiceImpl plaidService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    void getAccountInfo() {
-    }
-
-    @Test
-    void getTransactions() {
-    }
 }
