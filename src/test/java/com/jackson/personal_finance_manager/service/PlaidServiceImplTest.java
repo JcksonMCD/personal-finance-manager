e0c9b4f5fe6eab3deca436224e7ac73cf1a0ab6d
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-17 16:13:30 +0100

    Fix: More mocking added to setup of service impl file.

--- src/test/java/com/jackson/personal_finance_manager/service/PlaidServiceImplTest.java
+++ src/test/java/com/jackson/personal_finance_manager/service/PlaidServiceImplTest.java
@@ -1,12 +1,16 @@
 package com.jackson.personal_finance_manager.service;
 
 import com.plaid.client.PlaidClient;
+import com.plaid.client.request.*;
+import com.plaid.client.response.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import retrofit2.Call;
+
 
 @SpringJUnitConfig
 public class PlaidServiceImplTest {
@@ -17,12 +21,22 @@ public class PlaidServiceImplTest {
     @Mock
     private PlaidAuthService plaidAuthService;
 
+    @Mock
+    private Call<ItemPublicTokenExchangeResponse> mockExchangeCall;
+
+    @Mock
+    private Call<AuthGetResponse> mockAuthGetCall;
+
+    @Mock
+    private Call<TransactionsGetResponse> mockTransactionsGetCall;
+
     @InjectMocks
-    private PlaidServiceImpl plaidService;
+    private PlaidServiceImpl plaidServiceImpl;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         MockitoAnnotations.openMocks(this);
     }
+
 
 }
