6bb58c034b72695ce0681865459ce4e092105c56
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-17 16:57:11 +0100

    Feat: Goal repository annotated and now extends JPA repository.

--- src/main/java/com/jackson/personal_finance_manager/repository/GoalRepository.java
+++ src/main/java/com/jackson/personal_finance_manager/repository/GoalRepository.java
@@ -1,4 +1,9 @@
 package com.jackson.personal_finance_manager.repository;
 
-public interface GoalRepository {
+import com.jackson.personal_finance_manager.model.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GoalRepository extends JpaRepository<Goal, Long> {
 }
