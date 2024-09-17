5d4a1403d20245025baa81e74b82f518c931ea38
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-17 16:59:11 +0100

    Feat: Set up for user repository test file complete.

--- src/test/java/com/jackson/personal_finance_manager/repository/GoalRepositoryTest.java
+++ src/test/java/com/jackson/personal_finance_manager/repository/GoalRepositoryTest.java
@@ -1,7 +1,27 @@
 package com.jackson.personal_finance_manager.repository;
 
+import com.jackson.personal_finance_manager.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class GoalRepositoryTest {
+@DataJpaTest
+public class GoalRepositoryTest {
 
+    @Autowired
+    private GoalRepository goalRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setName("Test User");
+        userRepository.save(user);
+    }
 }
