26012bf56768612b4bbe3041149f9c0b41a82468
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:47:33 +0100

    Feat: goal impl test file created.

---
+++ src/test/java/com/jackson/personal_finance_manager/service/GoalServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.jackson.personal_finance_manager.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoalServiceImplTest {
+
+    @Test
+    void saveGoal() {
+    }
+}
