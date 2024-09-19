b4626d7d5c65ac92b2b6512dd2404e6e0e0f0c50
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:49:21 +0100

    Feat: Mocking set up complete for goal service impl test file.

--- src/test/java/com/jackson/personal_finance_manager/service/GoalServiceImplTest.java
+++ src/test/java/com/jackson/personal_finance_manager/service/GoalServiceImplTest.java
@@ -1,10 +1,26 @@
 package com.jackson.personal_finance_manager.service;
 
+import com.jackson.personal_finance_manager.repository.GoalRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoalServiceImplTest {
+
+    @Mock
+    private GoalRepository goalRepository;
+
+    @InjectMocks
+    private GoalServiceImpl goalService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void saveGoal() {
