67f1f95fdd34d86f20248a5cc69cf5b7ebbc6cf6
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:55:07 +0100

    Feat: Autowired goal service field added to goal controller class.

--- src/main/java/com/jackson/personal_finance_manager/controller/GoalController.java
+++ src/main/java/com/jackson/personal_finance_manager/controller/GoalController.java
@@ -1,9 +1,14 @@
 package com.jackson.personal_finance_manager.controller;
 
+import com.jackson.personal_finance_manager.service.GoalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/goals")
 public class GoalController {
+
+    @Autowired
+    private GoalService goalService;
 }
