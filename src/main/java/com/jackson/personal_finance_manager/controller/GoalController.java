cfe26d5404350089e07cb3670a083d2c51dda925
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:56:35 +0100

    Feat: Goal controller post endpoint created.

--- src/main/java/com/jackson/personal_finance_manager/controller/GoalController.java
+++ src/main/java/com/jackson/personal_finance_manager/controller/GoalController.java
@@ -1,7 +1,12 @@
 package com.jackson.personal_finance_manager.controller;
 
+import com.jackson.personal_finance_manager.model.Goal;
 import com.jackson.personal_finance_manager.service.GoalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ public class GoalController {
 
     @Autowired
     private GoalService goalService;
+
+    @PostMapping
+    public ResponseEntity<Goal> createGoal(@RequestBody Goal goal) {
+        Goal savedGoal = goalService.saveGoal(goal);
+        return new ResponseEntity<>(savedGoal, HttpStatus.CREATED);
+    }
 }
