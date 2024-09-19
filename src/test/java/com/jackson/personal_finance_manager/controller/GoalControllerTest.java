8f146d57162a250beb0fe0d3bcb50fb1b37f85df
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:59:59 +0100

    Feat: Mocking set up for controller test file.

--- src/test/java/com/jackson/personal_finance_manager/controller/GoalControllerTest.java
+++ src/test/java/com/jackson/personal_finance_manager/controller/GoalControllerTest.java
@@ -1,10 +1,32 @@
 package com.jackson.personal_finance_manager.controller;
 
+import com.jackson.personal_finance_manager.service.GoalService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 class GoalControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private GoalService goalService;
+
+    @InjectMocks
+    private GoalController goalController;
 
     @Test
     void createGoal() {
