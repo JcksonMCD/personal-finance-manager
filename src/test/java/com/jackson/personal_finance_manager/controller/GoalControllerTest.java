da7966cc4fd96335d698bac53b1be8a3a7233353
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:57:40 +0100

    Feat: Goal controller test file created.

---
+++ src/test/java/com/jackson/personal_finance_manager/controller/GoalControllerTest.java
@@ -0,0 +1,12 @@
+package com.jackson.personal_finance_manager.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoalControllerTest {
+
+    @Test
+    void createGoal() {
+    }
+}
