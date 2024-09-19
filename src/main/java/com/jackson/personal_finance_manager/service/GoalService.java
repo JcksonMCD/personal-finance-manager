c5d4f02a8c63bcccb7afcff12e88b29b5bc00ab6
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 17:24:16 +0100

    Feat: Get all goals method created in goal service interface.

--- src/main/java/com/jackson/personal_finance_manager/service/GoalService.java
+++ src/main/java/com/jackson/personal_finance_manager/service/GoalService.java
@@ -2,10 +2,14 @@ package com.jackson.personal_finance_manager.service;
 
 import com.jackson.personal_finance_manager.model.Goal;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GoalService {
     Goal saveGoal(Goal goal);
 
     Optional<Goal> getGoalById(Long goalId);
+
+    List<Goal> getAllGoals();
+
 }
