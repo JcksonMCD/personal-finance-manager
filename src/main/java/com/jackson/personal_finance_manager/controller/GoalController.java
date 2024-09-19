7cb374b30dc3d1fa85402b02f909166b7eb3d8ee
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:54:13 +0100

    Feat: Controller annotations added to goal controller class.

--- src/main/java/com/jackson/personal_finance_manager/controller/GoalController.java
+++ src/main/java/com/jackson/personal_finance_manager/controller/GoalController.java
@@ -1,4 +1,9 @@
 package com.jackson.personal_finance_manager.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/goals")
 public class GoalController {
 }
