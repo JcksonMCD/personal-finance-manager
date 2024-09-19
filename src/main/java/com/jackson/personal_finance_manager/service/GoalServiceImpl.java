5de68e106d14887468a48aeffc453235ca046400
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:46:06 +0100

    Feat: Autowired goal repository field added to impl.

--- src/main/java/com/jackson/personal_finance_manager/service/GoalServiceImpl.java
+++ src/main/java/com/jackson/personal_finance_manager/service/GoalServiceImpl.java
@@ -1,11 +1,16 @@
 package com.jackson.personal_finance_manager.service;
 
 import com.jackson.personal_finance_manager.model.Goal;
+import com.jackson.personal_finance_manager.repository.GoalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GoalServiceImpl implements GoalService{
-    
+
+    @Autowired
+    private GoalRepository goalRepository;
+
     @Override
     public Goal saveGoal(Goal goal) {
         return null;
