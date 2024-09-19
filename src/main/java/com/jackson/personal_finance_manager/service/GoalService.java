b355ab79df3a8926a75275950cd3bbf02a0b0b8f
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 17:15:10 +0100

    Feat: Get goal by id method created in goal service interface.

--- src/main/java/com/jackson/personal_finance_manager/service/GoalService.java
+++ src/main/java/com/jackson/personal_finance_manager/service/GoalService.java
@@ -2,6 +2,10 @@ package com.jackson.personal_finance_manager.service;
 
 import com.jackson.personal_finance_manager.model.Goal;
 
+import java.util.Optional;
+
 public interface GoalService {
     Goal saveGoal(Goal goal);
+
+    Optional<Goal> getGoalById(Long goalId);
 }
