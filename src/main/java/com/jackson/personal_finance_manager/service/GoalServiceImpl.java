55ceaae98e039b4d108b801e51a492c5efb4028a
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-09-19 16:45:12 +0100

    Feat: @Service annotation added to impl and save goal method signiture also added.

--- src/main/java/com/jackson/personal_finance_manager/service/GoalServiceImpl.java
+++ src/main/java/com/jackson/personal_finance_manager/service/GoalServiceImpl.java
@@ -1,4 +1,13 @@
 package com.jackson.personal_finance_manager.service;
 
+import com.jackson.personal_finance_manager.model.Goal;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GoalServiceImpl implements GoalService{
+    
+    @Override
+    public Goal saveGoal(Goal goal) {
+        return null;
+    }
 }
